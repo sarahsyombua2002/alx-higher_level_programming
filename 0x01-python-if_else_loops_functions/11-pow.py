3d837e193f9ae0f10fd56a3f214626e08c070e12
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-09-07 10:13:57 -0700

    a ^ b

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def pow(a, b):
-
     # this will hold the result of the multiplication
     result = 1
     base = 1
     nb = 0
-  
-  # multiply a exactly b times
+    # multiply a exactly b times
     if b < 0:
         nb = b
         b = (-1) * b
