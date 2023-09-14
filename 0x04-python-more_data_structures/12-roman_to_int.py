3639cd1e5c110f4c8be9fc50e8b054a882bff322
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-09-14 02:45:24 -0700

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-
-# roman_to_int - converts a roman numeral to an integer.
 def roman_to_int(roman_string):
-    romans = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000,\
-	    'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
-
-    if roman_string and type(roman_string) == str:
-        num = 0
-        for i in range(len(roman_string)):
-            for j in romans.keys():
-                if roman_string[:] is j:
-                    num = romans[j]
-                    return num
-                if roman_string[i:i+1] is j:
-                    num += romans[j]
-                    break
-        return num
-    else:
+    if not isinstance(roman_string, str) or not roman_string:
         return 0
+
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    i = 0
+
+    while i < len(roman_string):
+        # Check for subtractive forms (e.g., IV, IX, XL, XC, etc.)
+        if i + 1 < len(roman_string) and roman_string[i:i+2] in romans:
+            num += romans[roman_string[i:i+2]]
+            i += 2  # Move two characters forward
+        else:
+            num += romans[roman_string[i]]
+            i += 1  # Move one character forward
+
+    return num
+
