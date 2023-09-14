64c84bb69eb383a38595e3d892a55a1d16f5b3b9
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-09-14 02:48:25 -0700

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,18 +3,31 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return 0
 
-    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    num = 0
-    i = 0
+    # Define the mapping of Roman numerals to integers
+    romans = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
 
-    while i < len(roman_string):
-        # Check for subtractive forms (e.g., IV, IX, XL, XC, etc.)
-        if i + 1 < len(roman_string) and roman_string[i:i+2] in romans:
-            num += romans[roman_string[i:i+2]]
-            i += 2  # Move two characters forward
+    num = 0  # Initialize the result
+    prev_value = 0  # Initialize the previous numeral value
+
+    # Start from the end of the Roman numeral
+    for char in reversed(roman_string):
+        # Check if the character is a valid Roman numeral
+        if char not in romans:
+            return 0  # Invalid Roman numeral
+
+        current_value = romans[char]
+
+        # Compare the current numeral value with the previous one
+        if current_value < prev_value:
+            num -= current_value  # Subtract if it's smaller
         else:
-            num += romans[roman_string[i]]
-            i += 1  # Move one character forward
+            num += current_value  # Add if it's equal or larger
+
+        prev_value = current_value  # Update the previous value
 
     return num
+
 
