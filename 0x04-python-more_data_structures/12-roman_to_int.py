04d55246a9b6717965e7a5973f8661e88157d7fd
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-09-14 03:12:12 -0700

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return 0
@@ -9,24 +10,22 @@ def roman_to_int(roman_string):
         'C': 100, 'D': 500, 'M': 1000
     }
 
-    num = 0  # Initialize the result
-    prev_value = 0  # Initialize the previous numeral value
+    num = 0
+    prev_value = 0
 
     # Start from the end of the Roman numeral
     for char in reversed(roman_string):
-        # Check if the character is a valid Roman numeral
         if char not in romans:
-            return 0  # Invalid Roman numeral
+            return 0
 
         current_value = romans[char]
 
         # Compare the current numeral value with the previous one
         if current_value < prev_value:
-            num -= current_value  # Subtract if it's smaller
+            num -= current_value
         else:
-            num += current_value  # Add if it's equal or larger
+            num += current_value
 
-        prev_value = current_value  # Update the previous value
-
+        prev_value = current_value
     return num
 
