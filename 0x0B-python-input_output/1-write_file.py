7f2e554343b87674d0f046e9365929b76fa23e79
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-11-16 03:14:22 -0800

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-# 1-number_of_lines.py
+"""
+    1-write_file: write_file()
+"""
 
-def number_of_lines(filename=""):
-    """gets the number of lines from filename
+
+def write_file(filename="", text=""):
     """
-    with open(filename, encoding='utf-8') as myFile:
-        return sum([1 for line in myFile])
+        write_file writes a string to a text file.
+        Args:
+            filename (str): name of file.
+            text (str): text to be written.
+        Returns: number of bytes written.
+    """
+    with open(filename, "w", encoding='utf-8') as a_file:
+        return a_file.write(text)
