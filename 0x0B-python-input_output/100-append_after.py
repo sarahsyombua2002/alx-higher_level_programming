3f08c1e4f39728b5900e95b77e04643a43c45df5
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-11-16 04:14:19 -0800

    Advance

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
+"""
+Module for append_after method.
+"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """appends after search string instances in file
-    """
-    with open(filename, 'r', encoding='utf-8') as myFile:
-        lines = myFile.readlines()
-
-    for i, line in enumerate(lines):
-        if search_string in line:
-            lines.insert(i + 1, new_string)
-    with open(filename, 'w', encoding='utf-8') as myFile:
-        content = "".join(lines)
-        myFile.write(content)
+    '''Method for inserting text after search string.'''
+    lines = []
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        i = 0
+        while i < len(lines):
+            if search_string in lines[i]:
+                lines[i:i + 1] = [lines[i], new_string]
+                i += 1
+            i += 1
+    with open(filename, "w", encoding="utf-8") as f:
+        f.writelines(lines)
--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,63 +1,42 @@
 #!/usr/bin/python3
-"""module for use in log debugging
+"""
+Module for log parsing scripts.
 """
 
 
 import sys
 
 
-class DebugLog:
-    """class for use in debugging stdin logs
-    """
-    def __init__(self):
-        self.lines = []
-        self.total_size = 0
-        self.codes_200 = 0
-        self.codes_401 = 0
-        self.codes_403 = 0
-        self.codes_404 = 0
-        self.codes_405 = 0
-        self.codes_500 = 0
-        self.__current_line = 0
+if __name__ == "__main__":
+    size = [0]
+    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-    def __str__(self):
-        string = "File size: " + str(self.total_size) + '\n'
-        string += "200: " + str(self.codes_200) + '\n'
-        string += "401: " + str(self.codes_401) + '\n'
-        string += "403: " + str(self.codes_403) + '\n'
-        string += "404: " + str(self.codes_404) + '\n'
-        string += "405: " + str(self.codes_405) + '\n'
-        string += "500: " + str(self.codes_500)
-        return string
+    def check_match(line):
+        '''Checks for regexp match in line.'''
+        try:
+            line = line[:-1]
+            words = line.split(" ")
+            size[0] += int(words[-1])
+            code = int(words[-2])
+            if code in codes:
+                codes[code] += 1
+        except ValueError:
+            pass
 
-    def update(self, iteration_n):
-        """updates current response numbers
-            with new set of lines
-        """
-        for i in range(self.__current_line, self.__current_line + iteration_n):
-            if "200" in self.lines[i]:
-                self.codes_200 += 1
-            elif "401" in self.lines[i]:
-                self.codes_401 += 1
-            elif "403" in self.lines[i]:
-                self.codes_403 += 1
-            elif "404" in self.lines[i]:
-                self.codes_404 += 1
-            elif "405" in self.lines[i]:
-                self.codes_405 += 1
-            elif "500" in self.lines[i]:
-                self.codes_500 += 1
-            words = line.split(' ')
-            self.total_size += int(words[-1])
-            self.__current_line += 1
-
-
-log = DebugLog()
-lines = []
-line = sys.stdin.readline()
-while line != "":
-    log.lines.append(line)
-    if len(log.lines) % 10 == 0:
-        log.update(10)
-        print(log)
-    line = sys.stdin.readline()
+    def print_stats():
+        '''Prints accumulated statistics.'''
+        print("File size: {}".format(size[0]))
+        for k in sorted(codes.keys()):
+            if codes[k]:
+                print("{}: {}".format(k, codes[k]))
+    i = 1
+    try:
+        for line in sys.stdin:
+            check_match(line)
+            if i % 10 == 0:
+                print_stats()
+            i += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
